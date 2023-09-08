0ac13618c40b87454a0b7e1a7678dacbb6567df9
vani <vani.suw@gmail.com>
2023-09-08 14:33:32 +0700

    ini project Java saya

---
+++ Latihan02.java
@@ -0,0 +1,15 @@
+import javax.swing.ToolTipManager;
+
+/**
+ * Latihan02
+ */
+public class Latihan02 {
+    public static void main(String[] args) {
+        System.out.println("*****IDENTITAS DIRI*****");
+        System.out.println("Nama    : Adinda Mirza Devani");
+        System.out.println("Absen   : 02");
+        System.out.println("Kelas   : 1A");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
